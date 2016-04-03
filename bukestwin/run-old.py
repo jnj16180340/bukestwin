36d3f4e7c7a54165f310df0cfb2b62b7f6a1dd9d
nate johnson <jnj.16180340@gmail.com>
2016-04-03 01:10:10 -0400

    changed dev server port to 8000 from 80

--- bukestwin/run-old.py
+++ bukestwin/run-old.py
@@ -1,5 +1,6 @@
+## b;ah
 #!flask/bin/python3
 
 from app import app
 # this should not be in debug mode for deployment
-app.run(debug=True, host='0.0.0.0', port=80)
+app.run(debug=True, host='0.0.0.0', port=8000)
